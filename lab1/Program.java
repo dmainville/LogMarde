8943a6323453fcce7ed390b83a911db8b72cfeac
David <captaingob@gmail.com>
2014-09-22 18:41:56 -0400

    ajout multiplication et division

--- lab1/Program.java
+++ lab1/Program.java
@@ -3,7 +3,13 @@ public class Program
 {
 	public static void main(String[] args)
 	{
-		SuiteChainee s = new SuiteChainee("","addition",1,3,10,false);
+		SuiteChainee s = new SuiteChainee("","addition",0,0,0,true);
+		
+		System.out.println("Multiplication : "+s.multiplication(0, 200));
+		System.out.println("Multiplication : "+s.multiplication(-10, 3));
+		
+		System.out.println("Division : "+s.division(31, 3));
+		System.out.println("Division : "+s.division(31, -3));
 		
 		s.save("suite.txt");
 	}
